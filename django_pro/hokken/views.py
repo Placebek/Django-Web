e22104073d63335664919e1b7c2b5879494b8ca1
Placebek <goblins.from.mars2003@gmail.com>
2024-05-06 23:18:30 +0500

    создал хтмл файл

--- django_pro/hokken/views.py
+++ django_pro/hokken/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  for i in range(10):
-    return HttpResponse("<h4>Проверка работы<h4>")
+  return render(request, 'hokken/index.html')
+
+def about(request):
+  return HttpResponse("<p>About us</p>")
   
