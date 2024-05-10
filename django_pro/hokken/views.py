f2a2d633ccbd4ca0153b002c748bb93b011eef9b
Placebek <goblins.from.mars2003@gmail.com>
2024-05-11 03:01:27 +0500

    Исправил URL и HTML нового приложения(Новости)

--- django_pro/hokken/views.py
+++ django_pro/hokken/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 def index(request):
   data = {
-    'title': 'Главная страница'
+    'title': 'Главная страница',
+    'values': ['set', '10', '10-12-2024']
   }
   return render(request, 'hokken/index.html', data)
 
 def about(request):
   return render(request, 'hokken/about.html')
+
   
