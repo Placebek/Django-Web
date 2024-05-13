d3179d207aafde8b7c7def82977c2542a3372a69
Placebek <goblins.from.mars2003@gmail.com>
2024-05-13 22:42:16 +0500

    Создал еще один шаблон, чтобы показывать новости по подробнее

--- django_pro/news/urls.py
+++ django_pro/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create_news, name='create_news')
+    path('create', views.create_news, name='create_news'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail')
 ]
